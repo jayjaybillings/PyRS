5a35851174e6f75fe0150ea171ed0142c45312ae
Andrei Savici <saviciat@ornl.gov>
2020-01-21 16:31:00 -0500

    Re # 272. Add duration to list of logs

--- pyrs/core/nexus_conversion.py
+++ pyrs/core/nexus_conversion.py
@@ -273,6 +273,7 @@ class NeXusConvertingApp(object):
         logs_to_keep = summary_generator.DEFAULT_BODY_TITLES
         logs_to_keep.append(SUBRUN_LOGNAME)
         logs_to_keep.append('start_time')
+        logs_to_keep.append('duration')
         RemoveLogs(Workspace=ws, KeepLogs=logs_to_keep)
 
         # Mask
